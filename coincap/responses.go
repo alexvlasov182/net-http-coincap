d05c68df4f639b32b048821b0e7757d674748c1f
Oleksandr Vlasov <drumlife182@gmail.com>
2024-06-12 09:51:31 +0200

    Initial project

---
+++ coincap/responses.go
@@ -0,0 +1,29 @@
+package coincap
+
+import "fmt"
+
+type assetsResponse struct {
+	Asset     []Asset `json:"data"`
+	Timestamp int64   `json:"timestamp"`
+}
+
+type assetResponse struct {
+	Asset     Asset `json:"data"`
+	Timestamp int64 `json:"timestamp"`
+}
+
+type Asset struct {
+	ID           string `json:"id"`
+	Rank         string `json:""`
+	Symbol       string `json:""`
+	Name         string `json:""`
+	Supply       string `json:""`
+	MaxSupply    string `json:""`
+	MarketCapUSD string `json:""`
+	VolumeUSD24h string `json:""`
+	PriceUSD     string `json:"priceUSD"`
+}
+
+func (d Asset) Info() string {
+	return fmt.Sprintf("[ID] %s | [Rank] %s | [Symbol] %s | [Name] %s | [Price] %s", d.ID, d.Rank, d.Symbol, d.Name, d.PriceUSD)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module net-http-api/coincap
+
+go 1.22.1
